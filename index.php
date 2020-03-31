febcd6c3ee96234350ba79940caeceb8a185a419
James Walton <jawalton@redhat.com>
2020-03-31 12:13:59 -0400

    clean up html

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<html>
+<head> 
+<title>Containerfile Build</title>
+</head>
+<body> 
+
 <?php
-  echo "Built from Containerfile!";
+  echo "<h2>Built from Containerfile!</h2>";
+  echo "<p>Welcome: ";
   echo $_SERVER['HTTP_USER_AGENT'];
+  echo "</p>";
 ?>
+
+</body>
+</html>
