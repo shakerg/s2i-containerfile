e78de5bd86c5f0d51aae78bb11ea3430e8536aac
James Walton <jawalton@redhat.com>
2020-04-02 09:05:33 -0400

    v2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body> 
 
 <?php
-  echo "<h2>Built from Containerfile!</h2>";
+  echo "<h2>Built from Containerfile v2!</h2>";
   echo "<p>Welcome: ";
   echo $_SERVER['HTTP_USER_AGENT'];
   echo "</p>";
